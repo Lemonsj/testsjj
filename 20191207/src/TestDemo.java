887463887f0f55d1407db50c10422482c091242d
Lemonsj <1480845939@qq.com>
2019-12-07 23:47:42 +0800

    日常练习

---
+++ 20191207/src/TestDemo.java
@@ -0,0 +1,54 @@
+public class TestDemo {
+    /*CREATE DATABASE java20;
+    SHOW DATABASES;
+    create table students (
+            name VARCHAR(20),
+    age INT,
+    sn INT
+    );
+    INSERT INTO EXAM_GRADE(NAME,CHINESE,MATH,ENGLISH)VALUES
+         ('猪悟能', 88, 98, 90),
+         ('曹孟德', 82, 84, 67),
+         ('孙悟空', 88, 78, 77);
+
+    create table threads(
+            name VARCHAR(20) COMMENT '姓名',
+    content VARCHAR(100) COMMENT '内容',
+    time DATETIME
+  );
+
+
+
+
+    create table commodity(
+            name VARCHAR(20) comment '商品名称',
+    price INT,
+    inventory INT,
+    content VARCHAR(20)
+  );
+    insert into commodity(name,price,inventory,content) VALUES
+  ('钢笔', 20, 100, '写字'),
+          ('外套', 200, 150, '服装'),
+          ('课外书', 15, 80, '阅读');
+    select *from commodity;
+
+
+
+
+    create table teacher(
+            name VARCHAR(20),
+    age INT,
+    height INT,
+    weight INT,
+    sex VARCHAR(10),
+    academic VARCHAR(10),
+    birthday DATETIME,
+    IDnumber VARCHAR(20)
+  );
+
+    insert into teacher(name, age, height, weight, sex, academic, birthday, IDnumber)VALUES
+  ('李萍', 28, 165, 100, '女', '研究生', '1991-10-12 18:20:00', '612312199110120028'),
+          ('张伟', 34, 180, 140, '男', '研究生', '1985-01-19 13:59:00','612312198501192347'),
+          ('刘红', 26, 160, 98, '女', '博士', '1993-12-16 23:59:59','612312199312160045');
+    select *from teacher;*/
+}
