add7fa3a22f2b3958a01ab7f99520a7f18d57ad7
Lemonsj <1480845939@qq.com>
2020-03-11 21:49:18 +0800

    日常练习

---
+++ 20200311/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    synchronized void 普通方法() {
+        //同步方法
+    }
+    synchronized  static void 静态方法() {
+        //
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
