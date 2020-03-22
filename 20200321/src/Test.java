a6f5497af6f1c2f1c6d3f95856c49b53d1634164
Lemonsj <1480845939@qq.com>
2020-03-22 23:33:01 +0800

    日常练习

---
+++ 20200321/src/Test.java
@@ -0,0 +1,20 @@
+class Person {
+    String name = "No name";
+    public Person(String nm) {
+        name = nm;
+    }
+    public Person(){}
+}
+class Employee extends Person {
+    String empId = "0000";
+    public Employee(String id) {
+        //super("name");
+        empId = id;
+    }
+}
+public class Test {
+    public static void main(String args[]) {
+        Employee e = new Employee("123");
+        System.out.println(e.empId);
+    }
+}
