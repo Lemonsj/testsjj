84661449d2eda7c41d783abe2655b336148b260b
Lemonsj <1480845939@qq.com>
2020-03-27 23:56:29 +0800

    日常练习

---
+++ 20200327/src/Test.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        while(scan.hasNext()) {
+//            String str1 = scan.nextLine();
+//            char[] ptr = str1.toCharArray();
+//            for(int i = 0; i < ptr.length; i++) {
+//                ptr[i] =
+//            }
+//        }
+//    }
+//}
